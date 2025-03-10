e26a8a723d4bc593f69fac427a94f91cdd6e6c1f
liadraetta <lia.draetta@gmail.com>
2025-03-10 18:40:12 +0100

    src update

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import subprocess
 #inserire path completo!
 query_path = "/Users/liadraetta/Desktop/community-research-ontology/queries/work_query.sparql"
 output_path = "/Users/liadraetta/Desktop/community-research-ontology/output/nt_output/pubblications.ttl"
-jar_path = "../sparql-anything.jar"  # Update with the correct JAR file name
+jar_path = "../sparql-anything.jar"
 
 command = [
     "java", "-jar", jar_path,
@@ -13,7 +13,3 @@ command = [
 ]
 
 result = subprocess.run(command, capture_output=True, text=True)
-if result.returncode == 0:
-    print(f"SPARQL Anything executed successfully. Output saved to {output_path}")
-else:
-    print(f"Error: {result.stderr}")
